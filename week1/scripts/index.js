80b8abb49e256f9e9e16f273751b9756adf9a874
Preston Gunter <prestongunter@yahoo.com>
2021-04-23 14:29:59 -0600

    Week1 update

---
+++ week1/scripts/index.js
@@ -0,0 +1,53 @@
+const links = [
+  
+    {
+      label: "WDD 330 Home Page",
+      url: "../week1/homepage.html"
+    },
+
+    {
+      label: "Week 1 notes",
+      url: "../week1/index.html"
+    },
+
+    {
+        label: "Week 2 notes",
+        url: "../week2/index.html"
+    }
+  ];
+
+
+//BETTER
+//Get pointer to ul tag
+let ul = document.querySelector('#myDropdown');
+  links.forEach(
+      link => {
+        ul.innerHTML += 
+        `
+            <li>
+                <a href="${link.url}">${link.label}</a>
+            </li>
+        
+        `;
+      }
+  );
+
+
+// My DROP DOWN MENU
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
